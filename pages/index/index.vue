d3f755f30b789d16693c47fd83bc69e7fef26b58
wisdomfire <468179220@qq.com>
2022-06-21 01:54:29 +0800

    '0.9'

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -27,7 +27,6 @@
 					<view class="tab">
 							<view class="date" v-for="(item,index) in dates"  @click="chooseAddNum" :data-index="item" :class="{active:list==item.key}">{{item.date}} {{item.week}}</view>
 					</view>
-					
 						<scroll-view class="tab2" scroll-y="true" scroll-with-animation="true">
 							<view class="item2" v-for="(item,index) in resarrl" :key="index" v-show="num==0" >
 							   <view class="able"  v-show="outdatel">{{item.pt}}<br/>{{item.rem}}</view>
@@ -38,8 +37,6 @@
 								<view class="disable" v-show="outdates==false">{{item.pt}}<br/>{{item.rem}}<br/>无法预定</view>
 							</view>
 						</scroll-view>
-					
-						
 				</view>
 					
 					
@@ -54,7 +51,7 @@
 
 <script>
 	import data from '../../data.js';
-	import myfunction from '../../commons/js/myfunction.js';
+	
 	export default {
 		data() {
 			return {
@@ -66,31 +63,78 @@
 				arr1:[],
 				arr2:[],
 				arr3:[],
+				newarr3l:[],
+				newarr3s:[],
 				arr4:[],
+				newarr4l:[],
+				newarr4s:[],
 				arr5:[],
-				res:[],
+				newarr5l:[],
+				newarr5s:[],
 				resarrl:[],
 				resarrs:[],
-				dates:[{'date':'05-16','week':'一','key':0},{'date':'05-17','week':'二','key':1},{'date':'05-18','week':'三','key':2},{'date':'05-19','week':'四','key':3},{'date':'05-20','week':'五','key':4},{'date':'05-21','week':'六','key':5},{'date':'05-22','week':'日','key':6},{'date':'05-23','week':'一','key':7},{'date':'05-24','week':'二','key':8}],
+				dates:[{'date':'05-16','week':'一','key':0},
+				{'date':'05-17','week':'二','key':1},
+				{'date':'05-18','week':'三','key':2},
+				{'date':'05-19','week':'四','key':3},
+				{'date':'05-20','week':'五','key':4},
+				{'date':'05-21','week':'六','key':5},
+				{'date':'05-22','week':'日','key':6},
+				{'date':'05-23','week':'一','key':7},
+				{'date':'05-24','week':'二','key':8},
+				{'date':'05-25','week':'三','key':9},
+				{'date':'05-26','week':'四','key':10}],
 				dateone:[]
 			}
 		},
 		 computed: {  
+			 //按需合并日,周,季菜单
+			 //中午的菜单
 		      arrLunch: function () {  
-						  return this.res=this.arr3.filter(item => {
-						  return  item.meal=='中餐'&& item.dt=="2022-"+this.dateone.date
-					  })
+				   //日菜单
+				  this.newarr3l=this.arr3.filter(item => {
+				  return  item.meal=='中餐'&& item.dt=="2022-"+this.dateone.date
+			  })
+			  //周菜单
+			     this.newarr4l=this.arr4.filter(item => {
+			  	return item.meal=='中餐'&& item.beg_incl<="2022-"+this.dateone.date&&item.end_excl>="2022-"+this.dateone.date &&item.wk[0]<= this.chinesetonum(this.dateone.week)&&item.wk[1]>= this.chinesetonum(this.dateone.week)
+			  })
+			  //季菜单
+			    this.newarr5l= this.arr5.filter(item => {
+			    return item.meal=='中餐'&& item.beg_incl<="2022-"+this.dateone.date
+			   })
+			   //合并
+			   return this.newarr3l.concat(...this.newarr4l).concat(...this.newarr5l)
 			},
+			
+			//按需合并日,周,季菜单
+			//晚上的菜单
 			  arrSupper: function () {
-			    return  this.arr3.filter(item => {
+				  //日菜单
+			       this.newarr3s= this.arr3.filter(item => {
 			    	return item.meal=='晚餐'&& item.dt=="2022-"+this.dateone.date
-			     }) 
+			     })
+				 //周菜单
+				    this.newarr4s=this.arr4.filter(item => {
+			    	return item.meal=='晚餐'&& item.beg_incl<="2022-"+this.dateone.date&&item.end_excl>="2022-"+this.dateone.date &&item.wk[0]<= this.chinesetonum(this.dateone.week)&&item.wk[1]>= this.chinesetonum(this.dateone.week)
+			     })
+				 //季菜单
+				   this.newarr5s= this.arr5.filter(item => {
+				   return item.meal=='晚餐'&& item.beg_incl<="2022-"+this.dateone.date
+				  })
+				  //合并
+				  return this.newarr3s.concat(...this.newarr4s).concat(...this.newarr5s)
+				   
+				 
 			  } ,
+			//计算订餐截止时间
+			//中餐截止时间
 			outdatel: function () {
 				this.lunchout= this.arr1[0].tm_beg+this.arr1[0].tm_end*24*3600
 			    this.lunchoutdate=(this.list*24+11)*3600
 				return this.lunchout>this.lunchoutdate
 			  } ,
+			  //晚餐截止时间
 			outdates: function () {
 				this.supperout= this.arr1[1].tm_beg+this.arr1[1].tm_end*24*3600
 			    this.supperoutdate=(this.list*24+16)*3600
@@ -104,12 +148,11 @@
 		 this.mealarr3();
 		 this.mealarr4();
 		 this.mealarr5();
-		 this.dateone=this.dates[0];
-		 this.list=0;
-		 this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-		 this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
+		 this.clickCompute(0)
 		},
 		methods: {
+			
+			//合并菜单函数
 			//formArray:主数组，compareArray：需要联合的数组，key：比对的属性，isExpand是否将属性展开到item中
 			compareArrayUnion(formArray, compareArray, key,isExpand) {
 			  const compareObj = compareArray?.length
@@ -135,80 +178,93 @@
 			    : [];
 			  return resultArray;
 			},
-		
 			
-			//获取点击对象
+		   //合并相同项
+		   unique(arr){
+			
+				for(let i=0;i<arr.length;i++)
+					for(let j=i+1;j<arr.length;j++){
+					if(arr[i].pt === arr[j].pt){
+					 arr.splice(j,1);            
+					j--;
+					 }
+					}
+				return arr;
+		   },
+		   
+		   //菜单排序
+		   compare(a,b){
+			   return a.order_by-b.order_by
+		   },
+		   //汉字转数字
+		   chinesetonum(a){
+			   let changech = ['一', '二', '三', '四', '五', '六', '日'];
+			   switch(a){
+			   			case "一":
+			   		     return 0;
+			   			case "二":
+			   			 return 1;
+			   			case "三":
+			   			 return 2;
+			   			case "四":
+			   			 return 3;
+			   			case "五":
+			   			return 4;
+			   			case "六":
+			   			return 5;
+			   			case "日":
+			   			return 6;
+		   }
+			},
+			//点击后的菜单生成函数
+			clickCompute(num){
+				//获取选中日期
+				this.dateone=this.dates[num]
+				this.list=num
+				//生成当天菜单
+				//中餐
+				this.resarrl=this.unique(this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true))
+				//晚餐
+				 this.resarrs=this.unique(this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true))
+				 //菜单排序
+				  this.resarrl.sort(this.compare)
+				  this.resarrs.sort(this.compare)
+			},
+			//获取点击对象日期并显示菜单
 			chooseAddNum(e) {
 				
-				// for(let i=0 ;i<e.currentTarget.dataset.index.length;i++){
-				// 	if(e.currentTarget.dataset.index.key==i){
-				// 				this.dateone=this.dates[i]
-				// 				this.list=i
-				// 				console.log(this.dates[i])
-							
-				// 	}
-				// }
+			let num= e.currentTarget.dataset.index.key 
 				
-				if(e.currentTarget.dataset.index.key==0){
-							this.dateone=this.dates[0]
-							this.list=0
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							 this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-							// console.log(this.outdate)
-				}else if(e.currentTarget.dataset.index.key==1){
-							this.dateone=this.dates[1]
-							this.list=1
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-						     this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				}else if(e.currentTarget.dataset.index.key==2){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[2]
-							this.list=2
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				}else if(e.currentTarget.dataset.index.key==3){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[3]
-							this.list=3
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				}else if(e.currentTarget.dataset.index.key==4){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[4]
-							this.list=4
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				}else if(e.currentTarget.dataset.index.key==5){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[5]
-							this.list=5
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				}else if(e.currentTarget.dataset.index.key==6){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[6]
-							this.list=6
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				
-				}else if(e.currentTarget.dataset.index.key==7){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[7]
-							this.list=7
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
-				}else if(e.currentTarget.dataset.index.key==8){
-							// this.dates[2].date='2022-05-18'
-							this.dateone=this.dates[8]
-							this.list=8
-							this.resarrl=this.compareArrayUnion(this.arrLunch,this.arr2,"pt",true)
-							this.resarrs=this.compareArrayUnion(this.arrSupper,this.arr2,"pt",true)
+				if(num==0){
+					this.clickCompute(0)
+				}else if(num==1){
+					this.clickCompute(1)
+						
+				}else if(num==2){
+					this.clickCompute(2)
+						
+				}else if(num==3){
+					this.clickCompute(3)		 
+				}else if(num==4){
+					this.clickCompute(4)
+				}else if(num==5){
+					this.clickCompute(5)
+				}else if(num==6){
+					this.clickCompute(6)
+				}else if(num==7){
+					this.clickCompute(7)
+				}else if(num==8){
+					this.clickCompute(8)
+				}else if(num==9){
+					this.clickCompute(9)
+				}else if(num==10){
+					this.clickCompute(10)
 				}
          },
 		
 		 //处理时间
 		 // changeTime:function(date){
-		 // 	return myfunction.dateTime1(date)
+		 // 
 		 // },
 		 
 		 //获取数据
